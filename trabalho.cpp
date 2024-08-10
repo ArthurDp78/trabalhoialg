12b9a5573bd131b45717605da9e2ecc7df7d9957
ArthurDp78 <arthursoaresmarques78@gmail.com>
2024-08-10 12:16:39 -0300

    Update trabalho.cpp

--- trabalho.cpp
+++ trabalho.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -11,6 +12,9 @@ struct animais{
 	string classe;
 	short vida;
 };
+
+	
+
 void leitura(ifstream &arquivo, animais* exoticos,int &tamanho){
 	char lixo;
 	int i=0;
@@ -21,6 +25,7 @@ void leitura(ifstream &arquivo, animais* exoticos,int &tamanho){
 		getline(arquivo, exoticos[i].nome,',');
 		getline(arquivo, exoticos[i].pais,',');
 		arquivo >> exoticos[i].classe;
+		arquivo >> lixo;
 		arquivo >> exoticos[i].vida;
 		arquivo.ignore(); //pula fim de linha
 		
@@ -47,6 +52,7 @@ void leitura(ifstream &arquivo, animais* exoticos,int &tamanho){
 			getline(arquivo, exoticos[i].nome,',');
 			getline(arquivo, exoticos[i].pais,',');
 			arquivo >> exoticos[i].classe;
+			arquivo >> lixo;
 			arquivo >> exoticos[i].vida;
 			arquivo.ignore(); //pula fim de linha
 		
@@ -55,23 +61,117 @@ void leitura(ifstream &arquivo, animais* exoticos,int &tamanho){
 	}	
 	tamanho = i-1;
 }
-
-void saida(animais* exoticos, int tamanho){
-	for(int k = 0; k <tamanho; k++){
-		cout << exoticos[k].identificador << ",";
-		cout << exoticos[k].nome << ",";
-		cout << exoticos[k].pais << ", ";
-		cout << exoticos[k].classe<<" " ;
-		cout << exoticos[k].vida << endl;
-	}
 	
+
+void saida(animais* exoticos, int tamanho,bool &erro){
+	cout << "Voce deseja mostrar todos os dados?"<< endl;
+	cout << "1 - Sim"<< endl;
+	cout << "2- Nao" << endl;
+	
+	int resposta=0;
+	cin >> resposta;
+	if(resposta == 1){
+		for(int k = 0; k<tamanho; k++){
+			cout << exoticos[k].identificador << ",";
+			cout << exoticos[k].nome << "," ;
+			cout << exoticos[k].pais << "," ;
+			cout << exoticos[k].classe << ",";
+			cout << exoticos[k].vida << endl;
+	}
 }
+	else if(resposta== 2){
+		int n=0;
+		int j=0;
+		system("cls");
+		cout << "De qual ate qual dado voce quer mostrar?"<< endl;
+		cout << endl;
+		cout << "Comeco: ";
+		cin >> n;
+		cout <<"Fim: ";
+		cin >> j;
+		cout << endl;
+		
+		for(int i=n-1; i<=j-1;i++){
+			cout << exoticos[i].identificador << ",";
+			cout << exoticos[i].nome << "," ;
+			cout << exoticos[i].pais << "," ;
+			cout << exoticos[i].classe << ",";
+			cout << exoticos[i].vida << endl;
+		}
+		cout << endl;
+	}
+	else{
+		erro = true;
+		
+	}
+}
+
+void adcionar(animais* exoticos,int &tamanho){
+	for (int i=tamanho;i<tamanho+1;i++){
+		cout << "Adcionar identificador: ";
+		cin >> exoticos[i].identificador;
+		cout << "Adcionar nome: ";
+		getline(cin,exoticos[i].nome);
+		cout << "Adcionar pais de origem: ";
+		getline(cin,exoticos[i].pais);
+		cin >> exoticos[i].pais;
+		cout << "Adcionar classe: ";
+		cin >> exoticos[i].classe;
+		cout << "Adcionar tempo de vida: ";
+		cin >> exoticos[i].vida;
+		}
+		 
+		
+}
+void menu(animais*exoticos, int tamanho,bool erro){
+	system("cls");
+	int n;
+	int resposta=0;
+	cout << "O que deseja fazer?" << endl;
+	cout << "1- Mostrar os dados do existentes no programa" << endl;
+	cout << "2- Adicionar dados no programa" << endl;
+	cout <<"5- Sair do programa" << endl;
+	cin >> n;
+	
+	
+	
+	system("cls");
+	if(n == 1){    //caso de saida
+		saida(exoticos,tamanho,erro);
+		cout << endl;
+		cout << "Deseja voltar ao menu principal?"<< endl;
+		cout << "1 - Sim" << endl;
+		cout << "2 - Nao" << endl;
+		
+		cin >> resposta;
+		
+		if(resposta ==1){
+		system("cls");
+		menu(exoticos,tamanho,erro);
+	}
+}
+	if(n == 2){	   //caso de adição de animais
+		adcionar(exoticos,tamanho);
+		system("cls");
+		menu(exoticos,tamanho,erro);
+		
+		}
+	if(n==5){ //caso de encerramento do programa
+		exit(5);
+	}
+	else{ //caso de erro
+		erro = true;
+		
+	}
+		
+}
+
 
 int main(){
 	ifstream arquivo("animais.csv");
 	int tamanho=40;
 	string linha;
-	
+	bool erro = false;
 	getline(arquivo,linha);//primeira linha
 	
 	
@@ -79,19 +179,27 @@ int main(){
 	
 	leitura(arquivo,exoticos,tamanho);
 	
-	arquivo.close();
+	menu(exoticos,tamanho,erro);
 	
-
-	short n;
-	cin >> n;
-	
-	if(n == 1){    //caso de saida
-		saida(exoticos,tamanho);
+	int resposta;
+	while(erro == 0){ //repetiçao do erro
+		cout << "Nao existe essa opcao no programa" << endl;
+		cout << endl;
+		cout << "Deseja voltar ao menu principal?"<< endl;
+		cout << "1 - Sim" << endl;
+		cout << "2 - Nao" << endl;
+		cin >> resposta;
+		
+		if(resposta == 1){
+			erro = false;
+			menu(exoticos,tamanho,erro);
+		}
+		else if(resposta == 2){
+			exit(2);
+		}
+		else{
+			system("cls");
+		}
 	}
-	
-	
-	else cout << "fon";
-	
-	
 	return 0;
 }
